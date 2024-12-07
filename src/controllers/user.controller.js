792c8a93882d69b9f5ecda3e711e46a7d35caa67
realshashwatmishra <shashwatmishra444@gmail.com>
2024-12-08 00:13:22 +0530

    fixed some errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -14,7 +14,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // check if user already exists - username, email
-  const existedUser = User.findOne({ $or: [{ username }, { email }] });
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
